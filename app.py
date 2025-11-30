8541624c2bcab65b06dd4a994ca4afd36876a534
Liam Humble <humblelee@live.ca>
2025-11-30 17:37:20 -0500

    fixing isconneted

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def main():
 
         def waitForConnection():
             print("DEBUG: waitForConnection running | isConnected =", conn.isConnected)
-            if conn.isConnected:
+            if conn.isConnected():
                 app.load_chat_history(peer_ip)
                 print("DEBUG: Connected detected, loading history...")
             else:
